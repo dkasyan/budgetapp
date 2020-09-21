27bfc253bc1205d4431e5b1c57729d9636bcaaea
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-21 20:03:11 +0200

    add while loop

--- dht11.py
+++ dht11.py
@@ -8,18 +8,22 @@ logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s', filen
 
 from datetime import datetime
 
+period = 900
+DHT11_pin = 23
 
 
 def get_mesure():
   time = datetime.now()
   sensor = Adafruit_DHT.DHT11
-  DHT11_pin = 23
   humidity, temperature = Adafruit_DHT.read_retry(sensor, DHT11_pin)
-  logging.info(f"Wilgotność {humidity} Temp {temperature}")
+  logging.info(f"Wilgotność {humidity} % Temp {temperature} *C")
 
 
 
  # dane = ('Temperature={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 
 get_mesure()
-  
+logic = True
+while logic == True:
+  get_mesure()
+  time.sleep(period)
